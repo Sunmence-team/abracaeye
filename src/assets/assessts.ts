a53a4bde3c41bf0cd46caa30e2e8717615c181e1
Sanni Mubarak <oluwatoyinayomide6@gmail.com>
2025-11-15 15:57:08 +0100

    mobile

--- src/assets/assessts.ts
+++ src/assets/assessts.ts
@@ -1,10 +1,12 @@
 import logo from "./images/logo.png"
+import logo2 from "./images/logo2.png"
 import news1 from "./images/news1.png"
 import news2 from "./images/news2.jpg"
 import news3 from "./images/news3.jpg"
 
 export const assets = {
     logo,
+    logo2,
     news1,
     news2,
     news3,
