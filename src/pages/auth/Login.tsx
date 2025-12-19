318f34b68200704ed73e48db977924972b19d8ed
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2025-12-19 13:51:55 +0100

    edit

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -76,9 +76,9 @@ const Login: React.FC = () => {
   return (
     <>
       <div className="relative z-1 h-screen w-full flex items-center justify-center overflow-y-hidden">
-        <div className="absolute -z-1 top-0 left-0 bg-[#FF7979]/10 rounded-br-full rounded-bl w-64 h-64 md:w-150 md:h-150 p-8">
-          <Link to={"/"}><img src={assets.logo} /></Link>
-        </div>
+        <Link to={"/"} className='fixed top-8 left-8'><img src={assets.logo} /></Link>
+        <div className="absolute -z-1 top-0 left-0 bg-[#FF7979]/10 rounded-br-full rounded-bl w-64 h-64 md:w-150 md:h-150"></div>
+
 
         <div className="flex flex-col justify-center gap-5 items-center w-full max-w-md px-4 h-screen z-80">
           <div className="text-center flex flex-col">
